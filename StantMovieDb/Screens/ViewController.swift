968d51c531c2c22b789494cbd39bd39607a2f42f
GDX <tpmf@ic.ufal.br>
2022-10-27 20:18:49 -0300

    Create the movies request

--- StantMovieDb/Screens/ViewController.swift
+++ StantMovieDb/Screens/ViewController.swift
@@ -11,9 +11,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        didRequestMovies()
     }
 
-
+    func didRequestMovies() {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.themoviedb.org"
+        components.path = "/4/list/1"
+        components.queryItems = [
+            URLQueryItem(name: "api_key", value: "f321a808e68611f41312aa8408531476"),
+        ]
+        guard let url = components.url else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            if let existentError = error {
+                // trata erro
+                print(existentError)
+            }
+            
+            guard let data = data else { return }
+            
+            let decoder = JSONDecoder()
+            do {
+                let movies = try decoder.decode(Movie.self, from: data)
+                print(movies)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
 }
 
