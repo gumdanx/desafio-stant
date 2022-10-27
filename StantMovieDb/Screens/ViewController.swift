d5f5f78cad1dde0d4fcbda6a506a7e9c2d7a67fd
GDX <tpmf@ic.ufal.br>
2022-10-27 18:01:58 -0300

    Setup project

---
+++ StantMovieDb/Screens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StantMovieDb
+//
+//  Created by GDX on 27/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
